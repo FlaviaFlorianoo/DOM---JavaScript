71b06049abf382dca5a2322c857e7ce9b1337a65
FlaviaFlorianoo <113113009+FlaviaFlorianoo@users.noreply.github.com>
2023-01-24 21:58:56 -0300

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,23 @@
+let currentNumberWrapper = document.getElementById("currentNumber");
+const increment = document.getElementById("increment");
+const decrement = document.getElementById("decrement");
+
+let count = 0;
+
+increment.addEventListener("click", function increment() {
+  count++;
+  if (count <= 10) {
+    currentNumberWrapper.innerHTML = count;
+  } else if (count >= 0) {
+    currentNumberWrapper.classList.remove("negative");
+  }
+});
+
+decrement.addEventListener("click", function decrement() {
+  count--;
+  if (count >= -10) {
+    currentNumberWrapper.innerHTML = count;
+  } else if (count < 0) {
+    currentNumberWrapper.classList.add("negative");
+  }
+});
